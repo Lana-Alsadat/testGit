d499d37a4bcc4d4bd2d56de56fdc147f3b1d2a58
Lana ALsadat <lana.alsadat20@gmail.com>
2019-08-26 17:09:26 +0300

    1.Homework 'tree'

--- Desktop/java/tree/src/tree/tree.java
+++ Desktop/java/tree/src/tree/tree.java
@@ -3,32 +3,15 @@ package tree;
 public class tree {
 	 public static void main(String[] args) {
 		 
-	     /*  square(1,3);
-	       square(5,6);
-	       noun("hund");*/
-		 Tree(0,4);
+	    
+		 Tree(0,6);
 	
 	       }
 	 
-	 public static void square (int x , int y)
-	 {
-		 System.out.println("Square("+x+","+y+")");
-		for(int i=x;i<=y;i++)
-			System.out.println("> "+i+" - "+i*i);
-	 }
-	 
-	 public static void noun(String name)
-	 {
-		 boolean n1 = true;
-		 boolean n2 = false;
-		  if (name.endsWith("e"))
-			  System.out.println(n1);
-		  else 
-			  System.out.println(n2);
-	 }
+	
 	 public static void Tree(int x , int y)
 	 {
-     for(int i = x; i < y ;i++)
+     for(int i = x; i < y-1 ;i++)
      {
   	   for(int j = x ; j <= i ;j++)
   	   {
@@ -40,12 +23,14 @@ public class tree {
   		   
   	   else if (j % 2 != 0)
   	   System.out.print("y");
-  	
-  	  
+  	 
+  		 
+  	  		
+  	  	  
        }
+  	  System.out.println();
+  	
      }
-  	   System.out.println();
-  
+     System.out.print("M");
      }
   	   }
-}
