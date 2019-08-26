e22b48b902837c1801576fa33f20fc9a7df34b83
Lana ALsadat <lana.alsadat20@gmail.com>
2019-08-26 18:00:36 +0300

    Homework tree

---
+++ Desktop/java/HomeWork/src/tree.java
@@ -0,0 +1,35 @@
+
+public class tree {
+	 public static void main(String[] args) {
+		 
+		    
+		 Tree(0,6);
+	
+	       }
+	 
+	
+	 public static void Tree(int x , int y)
+	 {
+     for(int i = x; i < y-1 ;i++)
+     {
+  	   for(int j = x ; j <= i ;j++)
+  	   {
+  		   if(i==x)
+  			   System.out.print("I");
+  		   
+  		   else if (j % 2 == 0)
+  	   System.out.print("x");
+  		   
+  	   else if (j % 2 != 0)
+  	   System.out.print("y");
+  	 
+  		 
+  	  		
+  	  	  
+       }
+  	  System.out.println();
+  	
+     }
+     System.out.print("M");
+     }
+}
